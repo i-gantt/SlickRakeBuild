101a82cd041c974233d65c355dc7e77d5a5bc3dc
Cliff Burger <cliff.burger@gmail.com>
2013-04-18 10:22:12 -0700

    Stub out some specs to run or not run.

---
+++ example_products/bowling/spec/bowling_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+describe 'Bowling' do
+  it 'should add a meaningful test' do
+    true.should == true
+  end
+end
---
+++ example_products/cows_and_bulls/spec/cows_and_bulls_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+describe 'CowsAndBulls' do
+  it 'should add a meaningful test' do
+    true.should == true
+  end
+end
