66048d15d655960bac0c1e3581a79c65cad102e6
Cliff Burger <cliff.burger@gmail.com>
2013-04-19 10:02:51 -0700

    If remembered how to use the map function

--- slick-build/lib/git_commit_mapper.rb
+++ slick-build/lib/git_commit_mapper.rb
@@ -7,15 +7,8 @@ class GitCommitMapper
     commit.message = commit_source['commit']['message']
     file_entries = commit_source['files']
     commit.file_entries = file_entries
-    commit.file_names = extract_filenames(file_entries)
+    commit.file_names = file_entries.map { |f| f['filename'] }
     return commit
   end
 
-  def extract_filenames (file_entries)
-    parsed_files = []
-    file_entries.each do |f|
-      parsed_files << f['filename']
-    end
-    parsed_files
-  end
 end
