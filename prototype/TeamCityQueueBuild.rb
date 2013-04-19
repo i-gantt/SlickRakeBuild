8b2d27050805b2a97b10f0796afd74ff8b4090df
Cliff Burger <cliff.burger@gmail.com>
2013-04-18 20:11:00 -0700

    Strip refs/heads from the branch name

--- prototype/TeamCityQueueBuild.rb
+++ prototype/TeamCityQueueBuild.rb
@@ -17,8 +17,6 @@ class TeamCityQueueBuild
     @username = TeamCityQueueBuild::load_var!('TC_USERNAME')
     @password = TeamCityQueueBuild::load_var!('TC_PASSWORD')
     @base_url = TeamCityQueueBuild::load_var!('TC_BASE_URL')
-    @branch = ENV['branch']
-    @branch = 'master' if @branch.nil? || @branch.length == 0
     #@commit_hash = load_var('TC_COMMIT_HASH')
   end
 
@@ -28,10 +26,16 @@ class TeamCityQueueBuild
   # Requires a valid user with permissions to execute.
   def add_build_to_queue(build_type_id)
     action = 'add2Queue'
-    path = "/httpAuth/action.html?#{action}=#{build_type_id}&branchName=#{@branch}"
-    puts "Queue build #{build_type_id} on #{@branch} with #{path}"
-    execute_get_web_request(path,false) #generate_request(path)
+    path = "/httpAuth/action.html?#{action}=#{build_type_id}&branchName=#{get_branch()}"
+    puts "Queue build #{build_type_id} on #{get_branch()} with #{path}"
+    execute_get_web_request(path, false) #generate_request(path)
     puts 'Done with web request'
+  end
+
+  def get_branch
+    branch = ENV['branch']
+    return 'master' if branch.nil? || branch.length == 0
+    return branch.gsub(/refs\/heads\//, '')
   end
 
   def add_builds_to_queue(build_type_ids)
@@ -44,16 +48,16 @@ class TeamCityQueueBuild
 
   # Get the environment variables set by teamcity, or raise an exception if they are not set.
   def self.load_var!(name)
-      if (ENV.has_key? name) && (!ENV[name].nil?)
-        return ENV[name]
-      else
-        raise "TeamCity Environment variable [#{name}] is not set"
-      end
+    if (ENV.has_key? name) && (!ENV[name].nil?)
+      return ENV[name]
+    else
+      raise "TeamCity Environment variable [#{name}] is not set"
+    end
   end
 
   # Execute a web request
-  def execute_get_web_request(path,guest=false)
-    if(guest)
+  def execute_get_web_request(path, guest=false)
+    if guest
       username=GUEST_USER[:username]
       password=GUEST_USER[:password]
     else
@@ -64,13 +68,13 @@ class TeamCityQueueBuild
     puts "sending web request to #{uri.to_s}"
     http_obj=Net::HTTP.new(uri.host, uri.port)
     request=Net::HTTP::Get.new(uri.request_uri)
-    request.basic_auth(username,password)
+    request.basic_auth(username, password)
     http_obj.request(request).code
   end
 
   # Throws an exception if the request fails.
-  def execute_get_web_request!(path,guest=false)
-    response_code = execute_get_web_request(path,guest)
+  def execute_get_web_request!(path, guest=false)
+    response_code = execute_get_web_request(path, guest)
     if '200' != response_code
       raise('Web Request to TeamCity failed')
     else
