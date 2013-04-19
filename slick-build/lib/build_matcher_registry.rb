ba6267109de2c94c18c9fe952be7993cfd61ca7e
Cliff Burger <cliff.burger@gmail.com>
2013-04-19 13:07:54 -0700

    Put arrays where they are supposed to be in build mapper registry

--- slick-build/lib/build_matcher_registry.rb
+++ slick-build/lib/build_matcher_registry.rb
@@ -4,26 +4,21 @@ require_relative 'build_matcher'
 class BuildMatcherRegistry
   BowlingBuild = 'bt1597'
   CowBuild = 'bt1596'
-  PrototypeBuild = 'bt1596'
 
   def get_matchers
     return [bowling_matcher, cows_and_bulls]
   end
 
   def bowling_matcher
-    create_pattern_matcher(BowlingBuild, 'bowling', '#bowling')
+    create_pattern_matcher([BowlingBuild], 'bowling', '#bowling')
   end
 
   def cows_and_bulls
-    create_pattern_matcher(CowBuild, 'cows_and_bulls', '#cows')
-  end
-
-  def prototype
-    create_pattern_matcher(PrototypeBuild, 'prototype', '#prototype')
+    create_pattern_matcher([CowBuild], 'cows_and_bulls', '#cows')
   end
 
   def create_pattern_matcher(build_ids, file_pattern, message_pattern = '')
-    matcher = PatternBuildMatcher.new([build_ids])
+    matcher = PatternBuildMatcher.new(build_ids)
     matcher.file_pattern = file_pattern
     matcher.message_pattern = message_pattern
     return matcher
