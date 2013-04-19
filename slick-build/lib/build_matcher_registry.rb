d22b954335d4eee29165ef68e797f962a4f91cfd
Cliff Burger <cliff.burger@gmail.com>
2013-04-19 12:26:42 -0700

    Restore rakefile behavior -- queues specific builds, regardless of relevance

--- slick-build/lib/build_matcher_registry.rb
+++ slick-build/lib/build_matcher_registry.rb
@@ -2,21 +2,26 @@ require_relative 'build_matcher'
 
 #WARNING: I'm not complete. Please fix me.
 class BuildMatcherRegistry
+  BowlingBuild = 'bt1597'
+  CowBuild = 'bt1596'
+  PrototypeBuild = 'bt1596'
 
   def get_matchers
     return [bowling_matcher, cows_and_bulls]
   end
 
   def bowling_matcher
-    create_pattern_matcher(['bt1597'], 'bowling', '#bowling')
+    create_pattern_matcher([BowlingBuild], 'bowling', '#bowling')
   end
 
   def cows_and_bulls
-    create_pattern_matcher(['bt1596'], 'cows_and_bulls', '#cows')
+
+    create_pattern_matcher([CowBuild], 'cows_and_bulls', '#cows')
   end
 
   def prototype
-    create_pattern_matcher(['bt1596'], 'prototype', '#prototype')
+
+    create_pattern_matcher([PrototypeBuild], 'prototype', '#prototype')
   end
 
   def create_pattern_matcher(build_ids, file_pattern, message_pattern = '')
